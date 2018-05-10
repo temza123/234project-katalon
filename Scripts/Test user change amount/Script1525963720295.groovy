4bd15aff6385160707787672a8331670af48b76e
frong <zmack_tem@hotmail.com>
2018-05-10 21:51:47 +0700

    Create test user change amount case

---
+++ Scripts/Test user change amount/Script1525963720295.groovy
@@ -0,0 +1,44 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://34.208.44.70:8087/')
+
+WebUI.setText(findTestObject('Test user change amount/Page_ProjectBackend/input_username'), 'user')
+
+WebUI.setText(findTestObject('Test user change amount/Page_ProjectBackend/input_password'), 'user')
+
+WebUI.click(findTestObject('Test user change amount/Page_ProjectBackend/button_Login'))
+
+WebUI.click(findTestObject('Test user change amount/Page_ProjectBackend/button_add to cart'))
+
+WebUI.click(findTestObject('Test user change amount/Page_ProjectBackend/a_Carts            1'))
+
+WebUI.setText(findTestObject('Test user change amount/Page_ProjectBackend/input_amount'), '3')
+
+WebUI.click(findTestObject('Test user change amount/Page_ProjectBackend/p_Total price  60000 THB'))
+
+WebUI.click(findTestObject('Test user change amount/Page_ProjectBackend/button_confirm'))
+
+WebUI.closeBrowser()
+
